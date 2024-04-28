5dab1971264668a0d28834461f72a82f79afd87a
James DivineMercy <iamoluchimercy@gmail.com>
2024-04-28 17:40:17 +0100

    new commit

--- src/MoveZeros.java
+++ src/MoveZeros.java
@@ -3,23 +3,22 @@ import  java.util.Arrays;
 
 public class MoveZeros {
     public void moveZeros(int[] nums){
-        ArrayList<Integer> move = new ArrayList<>();
-
+        int[] newNum = new int[nums.length];
+        int count =0;
         for (int num : nums) {
             if (num != 0) {
-                move.add(num);
+                newNum[count] = num;
+                count++;
             }
         }
 
-        for (int num : nums) {
-            if (num == 0) {
-                move.add(num);
-            }
-        }
-        System.out.print(move);
+        System.arraycopy(newNum, 0, nums, 0, nums.length);
+
+
+        System.out.print(Arrays.toString(nums));
     }
 
     public static void main(String[] args) {
-        new MoveZeros().moveZeros(new int[]{0,1,0,3,12});
+        new MoveZeros().moveZeros(new int[]{0,13,12});
     }
 }
