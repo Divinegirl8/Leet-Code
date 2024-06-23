b17c296f19e0708010506017c99f9f933f720f69
James DivineMercy <iamoluchimercy@gmail.com>
2024-06-23 20:07:11 +0100

    new commit

--- src/Dob.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Dob {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("please enter your date of birth(DD-MM-YEAR): ");
-        String input = scanner.nextLine();
-    }
-}
